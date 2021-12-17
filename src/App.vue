180ba5aa5fda64d983c7c8d8c9c8a64789a2aaae
Oliver Steidel <oliversteidel@yahoo.de>
2021-12-17 11:38:29 +0100

    Home.vue: content, style

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,58 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view />
 </template>
 
 <style lang="scss">
 @use "./style/scss/setup/index" as *;
 #app {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
+  // font-size
+
+  h1 {
+    font-size: $fz-500;
+    font-weight: 400;
+  }
+
+  h2 {
+    font-size: $fz-400;
+  }
+
+  h3 {
+    font-size: $fz-300;
+  }
+
+  h4 {
+    font-size: $fz-200;
+  }
+
+  h5 {
+    font-size: $fz-100;
+    letter-spacing: 4.75px;
+  }
+
+  .subheading1 {
+    font-size: 1.75rem;
+  }
+
+  .subheading2 {
+    font-size: 0.875rem;
+    letter-spacing: 2.35px;
+  }
+
+  .nav-text,
+  .body-text {
+    font-size: 1rem;
+  }
+
+  .nav-text {
+    letter-spacing: 2.7px;
+  }
+
+  .body-text {
+    line-height: 25px;
+  }
 }
 
 #nav {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="wrapper">
+      <div class="home--content">
+        <h5>so, you want to travel to</h5>
+        <h1>space</h1>
+        <p class="body-text">
+          Let's face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we'll give you a truly out of this world
+          experience!
+        </p>
+      </div>
+      <div class="home--cta-container">
+        <button class="home--cta">EXPLORE</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "Home",
+};
+</script>
+
+<style lang="scss" scoped>
+@use "../style/scss/setup/index" as *;
+
+.home {
+  width: 100vw;
+  min-height: 100vh;
+  padding: 1.5rem;
+  background-image: url("../assets/home/background-home-mobile.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  &--content {
+    h5,
+    p {
+      color: $clr-purple;
+    }
+
+    h1 {
+      color: $clr-white;
+    }
+  }
+
+  &--cta-container {
+    button {
+      width: 9.375rem;
+      height: 9.375rem;
+      color: $clr-black;
+      background: $clr-white;
+      border: none;
+      border-radius: 50%;
+      font-size: 1.25rem;
+      font-weight: 400;
+      letter-spacing: 1.25px;
+    }
   }
 }
-</script>
+</style>
